282108f05747a23fb27b3bf7cb67c3b11b761d5d
yousefgog177 <48385135+yousefgog177@users.noreply.github.com>
2022-09-30 12:16:46 +0300

    Delete captcha.js

--- db/captcha.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const captcha = new mongoose.Schema({
-"id": { type: String } ,
-"captcha": { type: String } ,
-});
-
-module.exports = mongoose.model('Captcha', captcha, 'Captcha')
