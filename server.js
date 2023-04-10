6b8ebb828fcdf2898e80487db77ebac3db9fc6bb
priemum <50713962+priemum@users.noreply.github.com>
2023-04-10 16:50:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express')
 let fs = require('fs')
 let app = express()
 let client = require('./client/index.js')
-app.get('/', (req, res) =>{
+app.get('/', (req, res) =>{                 
 res.sendFile(__dirname + "/html.html")
 })
 /*
